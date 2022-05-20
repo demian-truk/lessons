eecc9180cbb39b1bba75552c763ab14c48646fd5
demian-truk <dimtruk93@gmail.com>
2022-05-21 00:01:15 +0300

    Add classwork

---
+++ lesson_03/classwork.py
@@ -0,0 +1,88 @@
+Exercise #1
+
+x = 17 / 2 * 3 + 2                                                                          
+print(x)                                                                      
+27.5
+
+x = 2 + 17 / 2 * 3                                                                          
+print(x)                                                                                             
+27.5
+
+x = 19 % 4 + 15 / 2 * 3                                                                     
+print(x)                                                                                              
+25.5
+
+x = (15 + 6) - 10 * 4                                                                       
+print(x)                                                                                              
+-19
+
+x = 17 / 2 % 2 * 3                                                                          
+print(x)                                                                                            
+1.5
+
+
+Exercise #2
+
+x = 17 / 2 * (3 + 2)                                                                       
+print(x)                                                                                             
+42.5
+
+x = (2 + 17) / 2 * 3                                                                       
+print(x)                                                                                
+28.5                                                                                                                                                   
+
+x = 19 % 4 + 15 / (2 * 3)                                                                  
+print(x)                                                                                         
+5.5
+
+x = ((15 + 6) - 10) * 4                                                                    
+print(x)                                                                                            
+44
+
+x = 17 / 2 % (2 * 3)                                                                       
+print(x)                                                                                            
+2.5
+
+
+Exercise #3
+
+alex = 20                                                                                  
+ben = 34                                                                                   
+john = 55                                                                                  
+print(alex + ben + john)                                                                   
+109
+
+
+Exercise #4
+
+result = (alex + ben + john) / 3                                                           
+print(result)                                                                              
+36.333333333333336
+
+
+Exercise #5
+
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]                              
+result = len(my_list) - len(set(my_list))
+print(result)                                                         
+3
+
+
+Exercise #6
+
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]                              
+result = my_list[2:5][::-1]                                                                
+print(result)                                                                              
+[5.0, 2, 2]
+
+
+Exercise #7
+
+import math                                                                                (
+
+size = 3                                                                                   
+list = [size * 4, size * size, math.sqrt(2) * size]                                            
+print(list)                                                                                
+[12, 9, 4.242640687119286]
+
+
