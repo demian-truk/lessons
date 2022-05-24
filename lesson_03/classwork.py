972d9711d21d6ad3ed7d142db2eee63997632575
demian-truk <dimtruk93@gmail.com>
2022-05-25 02:35:26 +0300

    Update classwork

--- lesson_03/classwork.py
+++ lesson_03/classwork.py
@@ -84,5 +84,3 @@ size = 3
 list = [size * 4, size * size, math.sqrt(2) * size]                                            
 print(list)                                                                                
 [12, 9, 4.242640687119286]
-
-
