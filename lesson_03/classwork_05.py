ec283627d1c7b08057f475096634711cd19ba61f
demian-truk <dimtruk93@gmail.com>
2022-05-26 00:14:47 +0300

    Update classwork_04

--- lesson_03/classwork_05.py
+++ lesson_03/classwork_05.py
@@ -1,6 +1,8 @@
-Exercise #5
+"""
+Найти в списке ниже количество не уникальных элементов:
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+"""
 
 my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
 result = len(my_list) - len(set(my_list))
 print(result)
-3
