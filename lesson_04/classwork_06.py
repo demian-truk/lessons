b27d2c0150d42be048f4636bb5c28c6b706d4614
demian-truk <dimtruk93@gmail.com>
2022-05-27 12:25:48 +0300

    Add classwork_06

---
+++ lesson_04/classwork_06.py
@@ -0,0 +1,20 @@
+"""
+Вывести в порядке возрастания все простые числа, расположенные между n и m (включая сами числа n и m).
+Также вывести количество x этих чисел.
+"""
+
+n = int(input("Введите первое число: "))
+m = int(input("Введите второе число: "))
+result = []
+
+for number in range(n, m+1):
+    if number > 1:
+        for num in range(2, number):
+            if number % num == 0:
+                break
+        else:
+            result.append(number)
+print("Список всех простых чисел в диапазоне:", result)
+
+x = len(result)
+print("Количество простых чисел:", x)
