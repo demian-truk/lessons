78669cc7ab55e179601d5c1eff98c8c44230dda4
demian-truk <dimtruk93@gmail.com>
2022-05-27 14:55:12 +0300

    Add classwork_07

---
+++ lesson_04/classwork_07.py
@@ -0,0 +1,41 @@
+"""
+Пользователь вводит с клавиатуры числа до тех пор, пока не введет любой строчный символ.
+Из этих чисел необходимо составить первый список. Далее таким же образом вводятся второй и третий списки.
+Вывести на экран список, элементы которого есть в первых двух списках, но отсутствуют в третьем.
+"""
+
+list1 = []
+list2 = []
+list3 = []
+
+# create a list of numbers
+
+while True:
+    n = input("Введите числа для 1-го списка (введите строку, чтобы закончить): ")
+    if not n.isdigit():
+        break
+    else:
+        list1.append(n)
+print("1-ый список:", list1)
+
+while True:
+    n = input("Введите числа для 2-го списка (введите строку, чтобы закончить): ")
+    if not n.isdigit():
+        break
+    else:
+        list2.append(n)
+print("2-ой список:", list2)
+
+while True:
+    n = input("Введите числа для 3-го списка (введите строку, чтобы закончить): ")
+    if not n.isdigit():
+        break
+    else:
+        list3.append(n)
+print("3-ий список:", list3)
+
+# print a list of numbers in the first two lists but missing in the third
+
+result = set(list1) & set(list2)
+result = set(result) - set(list3)
+print("Новый список:", list(result))
