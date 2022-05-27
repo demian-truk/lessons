74c6729a929a4da37beee1d4508f79b9960ad409
demian-truk <dimtruk93@gmail.com>
2022-05-27 21:13:41 +0300

    Add classwork_01

---
+++ lesson_05/classwork_01.py
@@ -0,0 +1,11 @@
+"""
+Написать функцию, которая получает на вход имя и выводит строку вида: f"Hello, {name}".
+Создать список из 5 имен. Вызвать функцию для каждого элемента списка в цикле.
+"""
+
+def format_string(name):
+    print(f"Hello, {name}")
+
+my_names = ["Alex", "Sandra", "Ben", "Johnny", "Pit"]
+for name in my_names:
+    format_string(name)
