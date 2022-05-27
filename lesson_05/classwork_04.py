7fd16e809b1dbfa64dd846602bd795e9b6a289bf
demian-truk <dimtruk93@gmail.com>
2022-05-27 21:56:07 +0300

    Add classwork_04

---
+++ lesson_05/classwork_04.py
@@ -0,0 +1,18 @@
+"""
+Написать функцию month_to_season(), которая принимает 1 аргумент - номер месяца.
+Функция возвращает название сезона, к которому относится этот месяц.
+Например, передаем 2, на выходе получаем "Winter".
+"""
+
+def month_to_season(month):
+    mapping = {
+        "Winter": [1, 2, 12],
+        "Spring": [3, 4, 5],
+        "Summer": [6, 7, 8],
+        "Autumn": [9, 10, 11],
+    }
+    for season, month_list in mapping.items():
+        if month in month_list:
+            return season
+
+print(month_to_season(7))
