94d793362b5898ac255938b2224f93daf09c30c4
demian-truk <dimtruk93@gmail.com>
2022-05-28 01:54:33 +0300

    Update classwork_05

--- lesson_04/classwork_05.py
+++ lesson_04/classwork_05.py
@@ -2,8 +2,8 @@
 Получить сумму кубов натуральных чисел от n до m, используя цикл for, числа n и m вводятся с клавиатуры.
 """
 
-n = int(input("Введите первое натуральное число: "))
-m = int(input("Введите второе натуральное число: "))
+n = int(input("Enter first natural number: "))
+m = int(input("Enter second natural number: "))
 sum_of_cubes_of_numbers = 0
 
 for number in range(n, m):
