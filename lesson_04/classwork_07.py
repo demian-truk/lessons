53449bc053a5cccaecfa59b7f6c4e41b5f119433
demian-truk <dimtruk93@gmail.com>
2022-05-28 02:00:36 +0300

    Update classwork_07

--- lesson_04/classwork_07.py
+++ lesson_04/classwork_07.py
@@ -8,29 +8,29 @@ list1 = []
 list2 = []
 list3 = []
 
-# create a list of numbers
+# create list of numbers
 while True:
-    number = input("Enter numbers for 1st list (enter a string to stop): ")
+    number = input("Enter numbers for 1st list (enter string to stop): ")
     if not number.isdigit():
         break
     else:
         list1.append(number)
 
 while True:
-    number = input("Enter numbers for 2nd list (enter a string to stop): ")
+    number = input("Enter numbers for 2nd list (enter string to stop): ")
     if not number.isdigit():
         break
     else:
         list2.append(number)
 
 while True:
-    number = input("Enter numbers for 3rd list (enter a string to stop): ")
+    number = input("Enter numbers for 3rd list (enter string to stop): ")
     if not number.isdigit():
         break
     else:
         list3.append(number)
 
-# print a list of numbers in the first two lists but missing in the third
+# print list of numbers in first two lists but missing in third
 new_list = set(list1) & set(list2)
 new_list = set(new_list) - set(list3)
 print("New list:", list(new_list))
