6864bef63b66ac83e61362836c3bb6cb318f4577
demian-truk <dimtruk93@gmail.com>
2022-06-01 12:12:32 +0300

    Update solution_03

--- lesson_05/solution_03.py
+++ lesson_05/solution_03.py
@@ -1,8 +1,14 @@
 """
-Написать функцию, которая используя модуль requests скачивает файл сохраняет его на файловой системе.
+Написать функцию, которая используя модуль requests скачивает файл и сохраняет его на файловой системе.
 Функция имеет два параметра: ссылка на файл и имя на файловой системе.
 В качестве примера ссылки на файл можно использовать лицензию из ГитХаба из вашего репозитория.
 """
 
-def download():
-    pass
+import requests
+
+def download_and_save(link, name):
+    with open(name, "wb") as file:
+        link = requests.get(link)
+        file.write(link.content)
+
+download_and_save("https://raw.githubusercontent.com/demian-truk/lessons/master/LICENSE", "License")
