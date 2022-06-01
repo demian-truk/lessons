d56774751bb01e91085f8e0a3edb85515b200653
demian-truk <dimtruk93@gmail.com>
2022-06-01 08:13:20 +0300

    Update classwork_01

--- lesson_05/classwork_01.py
+++ lesson_05/classwork_01.py
@@ -3,9 +3,10 @@
 Создать список из 5 имен. Вызвать функцию для каждого элемента списка в цикле.
 """
 
+my_names = ["Alex", "Sandra", "Ben", "Johnny", "Pit"]
+
 def format_string(name):
     print(f"Hello, {name}")
 
-my_names = ["Alex", "Sandra", "Ben", "Johnny", "Pit"]
 for name in my_names:
     format_string(name)
