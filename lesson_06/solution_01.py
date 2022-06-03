db92cccc0ee7469951c29f85478aada52c0166c6
demian-truk <dimtruk93@gmail.com>
2022-06-03 14:53:59 +0300

    Update solution_01

---
+++ lesson_06/solution_01.py
@@ -0,0 +1,16 @@
+"""
+Напишите программу, которая принимает текст и выводит два слова.
+Первое - это наиболее часто встречающееся слово, второе - самое длинное.
+В идеале не использовать библиотечные функции.
+"""
+
+my_text = "Hello, I'm Dima. I love computer games. Games take a lot of time. But it's a lot of fun. Games forever :)"
+my_text = my_text.split()
+
+
+def max_len_of_word_in_text(text):
+    max_len = max(my_text, key=len)
+    return max_len
+
+
+print(max_len_of_word_in_text(my_text))
