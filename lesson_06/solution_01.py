a6b5222d1e7a657454b9144676f91a104095e7f5
demian-truk <dimtruk93@gmail.com>
2022-06-03 19:35:26 +0300

    Update solution_01

--- lesson_06/solution_01.py
+++ lesson_06/solution_01.py
@@ -4,18 +4,21 @@
 В идеале не использовать библиотечные функции.
 """
 
-my_text = "Hello, I'm Dima. I love computer games. Games take a lot of time. But it's a lot of fun. Games forever :)"
-my_text = my_text.split()
+MIN_WORD_LENGTH = 3
 
 
 def most_common_word_in_text(text):
     pass
 
 
+# using function "max"
 def max_len_of_word_in_text(text):
-    max_len = max(my_text, key=len)
+    text = text.split()
+    max_len = max(text, key=len)
     return max_len
 
+
+my_text = "Hello, I'm Dima. I love computer games. Games take a lot of time. But it's a lot of fun. Games forever :)"
 
 print(most_common_word_in_text(my_text))
 print(max_len_of_word_in_text(my_text))    # computer
