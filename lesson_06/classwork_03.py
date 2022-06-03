37502b54c5cb91d402cfaa0f641d6ee5448ff0e8
demian-truk <dimtruk93@gmail.com>
2022-06-03 13:05:15 +0300

    Add classwork_03

--- lesson_06/classwork_03.py
+++ lesson_06/classwork_03.py
@@ -2,5 +2,18 @@
 Написать функцию, которая возвращают случайным образом одну карту из стандартной колоды в 36 карт.
 При этом, на первом месте номинал карты (6 - 10, J, D, K, A).
 На втором - название масти (Hearts, Diamonds, Clubs, Spades).
-
 """
+
+import random
+
+card_nominal = [6, 7, 8, 9, 10, "J", "D", "K", "A"]
+card_suit = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+
+def random_card():
+    random_nominal = random.choice(card_nominal)
+    random_suit = random.choice(card_suit)
+    return random_nominal, random_suit
+
+
+print(random_card())
