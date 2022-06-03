b459c5ad48d94f60edd4990bc94e9c76b1ce5923
demian-truk <dimtruk93@gmail.com>
2022-06-03 16:33:51 +0300

    Update classwork_05

--- lesson_03/classwork_05.py
+++ lesson_03/classwork_05.py
@@ -5,5 +5,5 @@ my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
 
 my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
 
-number_of_non_unique_elements = len(my_list) - len(set(my_list))
-print(number_of_non_unique_elements)    # 3
+amount_of_non_unique_elements = len(my_list) - len(set(my_list))
+print(amount_of_non_unique_elements)    # 3
