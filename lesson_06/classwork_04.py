93cba8f26b36ec79f9af4d458cd9bfb9e180794f
demian-truk <dimtruk93@gmail.com>
2022-06-03 14:15:55 +0300

    Update classwork_04

---
+++ lesson_06/classwork_04.py
@@ -0,0 +1,41 @@
+"""
+Используя функцию из предыдущей задачи, написать программу игру Блэкджек.
+Для этого необходимо реализовать цикл, в котором на каждом ходу у игрока спрашивается, достать ли следующую карту.
+В случае положительного ответа - вытягивать случайную карту.
+Игра заканчивается если игрок отказывается брать карту, либо сумма его карт больше 21-го.
+"""
+
+from classwork_03 import random_card
+import random
+
+card_deck = {
+    "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
+    "J": 2, "D": 3, "K": 4, "A": 1,
+}
+
+
+def card_value(nominal):
+    return card_deck[nominal]
+
+
+card_nominal, _ = random_card()
+value = card_value(card_nominal)
+current_sum = value
+
+while True:
+    choice = input("Get next card [Y/n]: ")
+    if choice == "n":
+        break
+
+    card_nominal, _ = random_card()
+    value = card_value(card_nominal)
+    current_sum += value
+
+    if current_sum > 21:
+        print(f"Game over! Your current amount: {current_sum}")
+        break
+    if current_sum == 21:
+        print("You win")
+        break
+    if current_sum < 21:
+        print(f"Your current amount: {current_sum}")
