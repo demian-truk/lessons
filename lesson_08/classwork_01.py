37899cba07949d0af87f15ab0efc8e9b60dbfb08
demian-truk <dimtruk93@gmail.com>
2022-06-09 01:35:43 +0300

    Update classwork_01

--- lesson_08/classwork_01.py
+++ lesson_08/classwork_01.py
@@ -2,24 +2,16 @@
 Создать класс Dog. Класс имеет четыре атрибута: height, weight, name, age.
 Класс имеет три метода: jump, run, bark. Каждый метод выводит сообщение на экран.
 Создать объект класса Dog, вызвать все методы объекта и вывести на экран все его атрибуты.
+
+# Upd - Программа имеет другой вид, получившийся после решения задания №4.
 """
 
+from classwork_04 import Animal
 
-class Dog:
 
-    def __init__(self, height: int, weight: int, name: str, age: int):
-        self.height = height
-        self.weight = weight
-        self.name = name
-        self.age = age
+class Dog(Animal):
 
-    def jump(self):
-        print(f"{self.name} is jumping")
-
-    def run(self):
-        print(f"{self.name} is running")
-
-    def bark(self):
+    def talk(self):
         print(f"{self.name} is barking")
 
 
@@ -27,5 +19,5 @@ if __name__ == "__main__":
     dog_1 = Dog(10, 5, "Bruce", 2)
     dog_1.jump()
     dog_1.run()
-    dog_1.bark()
+    dog_1.talk()
     print(f"Name: {dog_1.name}, height: {dog_1.height}, weight: {dog_1.weight}, age: {dog_1.age}")
