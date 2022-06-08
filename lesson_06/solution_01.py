3de90c7f92cea82fc743c88915157f82a0d20d03
demian-truk <dimtruk93@gmail.com>
2022-06-08 16:56:18 +0300

    Update solution_01

--- lesson_06/solution_01.py
+++ lesson_06/solution_01.py
@@ -4,21 +4,53 @@
 В идеале не использовать библиотечные функции.
 """
 
-MIN_WORD_LENGTH = 3
+MIN_WORD_LENGTH = 4
 
 
-def most_common_word_in_text(text):
-    pass
+def get_text_stats(text: str) -> dict:
+    text = text.lower()
+    text = text.replace(".", " ").replace(",", " ").split()
+    result = {}
+    for word in text:
+        word = word.strip()
+        if len(word) < MIN_WORD_LENGTH:
+            continue
+        if word in result:
+            result[word]["count"] += 1
+        else:
+            result[word] = {
+                "length": len(word),
+                "count": 1,
+            }
+    return result
 
 
-# using function "max"
-def max_len_of_word_in_text(text):
+def get_most_frequent_word(text_stats: dict) -> str:
+    most_frequent = list(text_stats.keys())[0]
+    for word, stats in text_stats.items():
+        if text_stats[most_frequent]["count"] < stats["count"]:
+            most_frequent = word
+    return most_frequent
+
+
+def get_the_longest_word(text_stats: dict) -> str:
+    longest = list(text_stats.keys())[0]
+    for word, stats in text_stats.items():
+        if text_stats[longest]["length"] < stats["length"]:
+            longest = word
+    return longest
+
+
+# get the longest word using function "max"
+"""
+def get_longest_word(text):
     text = text.split()
     max_len = max(text, key=len)
     return max_len
-
+"""
 
 my_text = "Hello, I'm Dima. I love computer games. Games take a lot of time. But it's a lot of fun. Games forever :)"
+my_stats = get_text_stats(my_text)
 
-print(most_common_word_in_text(my_text))
-print(max_len_of_word_in_text(my_text))    # computer
+print(f"Most frequent word in text: {get_most_frequent_word(my_stats)}")    # Most frequent word in text: games
+print(f"The longest word in text: {get_the_longest_word(my_stats)}")    # The longest word in text: computer
