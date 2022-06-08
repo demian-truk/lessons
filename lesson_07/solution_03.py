770f002d11c5f936be7a8492c2c3a66a61b1e508
demian-truk <dimtruk93@gmail.com>
2022-06-09 00:59:26 +0300

    Update solution_03

--- lesson_07/solution_03.py
+++ lesson_07/solution_03.py
@@ -12,34 +12,35 @@
 import csv
 
 
-def customer_purchases(sales):
-    sales_dict = {customer: [] for customer, product, amount, price in sales}
-    for customer, product, amount, price in sales:
-        sales_dict[customer].append([product, amount, price])
-    for key, value in sales_dict.items():
-        sum_of_amount_of_products = 0
-        cost_of_products = 0
-        res_dict = {}
-        res_list = []
-        for i in value:
-            sum_of_amount_of_products += int(i[1])
-            cost_of_products += int(i[1]) * int(i[2])
-            res_list = [sum_of_amount_of_products, cost_of_products]
-        res_dict[key] = res_list
-        for k, v in res_dict.items():
-            print(f"{k}: total amount of products - {v[0]}, total cost of products - {v[1]}")
-
-
-def product_purchases(sales):
-    pass
-
-
-def main():
+def load_sales_from_file():
     sales = []
     with open("dictionary.csv", "r") as file:
         for row in csv.reader(file):
+            row[2], row[3] = int(row[2]), int(row[3])
             sales.append(row)
-    customer_purchases(sales)
+    return sales
+
+
+def get_stats(sales, index):
+    result = {}
+    for row in sales:
+        if row[index] in result:
+            result[row[index]]["products_bought"] += row[2]
+            result[row[index]]["total cost"] += row[2] * row[3]
+        else:
+            result[row[index]] = {
+                "products_bought": row[2],
+                "total cost": row[2] * row[3],
+            }
+    return result
+
+
+def main():
+    sales = load_sales_from_file()
+    user_stats = get_stats(sales, index=0)
+    print(f"Customer purchases: {user_stats}")
+    product_stats = get_stats(sales, index=1)
+    print(f"Product purchases: {product_stats}")
 
 
 if __name__ == "__main__":
