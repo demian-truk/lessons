4742d21d1f5e0365d97618b638e7dbe18696ee19
demian-truk <dimtruk93@gmail.com>
2022-06-09 01:41:03 +0300

    Update classwork_03

--- lesson_08/classwork_03.py
+++ lesson_08/classwork_03.py
@@ -1,16 +1,20 @@
 """
 Создать новый класс Cat, который имеет все те же атрибуты что и Dog, только заменить метод bark на meow.
+
+# Upd - Программа имеет другой вид, получившийся после решения задания №4.
 """
 
-from classwork_01 import Dog
+from classwork_04 import Animal
 
 
-class Cat(Dog):
+class Cat(Animal):
 
     def talk(self):
         print(f"{self.name} is meowing")
 
 
 if __name__ == "__main__":
-    cat_1 = Cat(7, 4, "Barsik", 1)
+    cat_1 = Cat(10, 5, "Elvis", 2)
+    cat_1.jump()
+    cat_1.run()
     cat_1.talk()
