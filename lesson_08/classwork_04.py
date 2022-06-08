90a9ac32b63b6ed5e481541b1b85a367cbf25d33
demian-truk <dimtruk93@gmail.com>
2022-06-09 01:25:48 +0300

    Update classwork_04

--- lesson_08/classwork_04.py
+++ lesson_08/classwork_04.py
@@ -3,3 +3,21 @@
 Унаследовать Dog и Cat от класса Animal и удалить в дочерних классах те методы, которые имеются у родительского класса.
 Создать объект каждого класса и вызвать все его методы.
 """
+
+
+class Animal:
+
+    def __init__(self, height: int, weight: int, name: str, age: int):
+        self.height = height
+        self.weight = weight
+        self.name = name
+        self.age = age
+
+    def jump(self):
+        print(f"{self.name} is jumping")
+
+    def run(self):
+        print(f"{self.name} is running")
+
+    def talk(self):
+        print(f"{self.name} is talking")
