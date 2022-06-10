9f6b1b78ed0ce8055f68c300d198ebbbc74ede32
demian-truk <dimtruk93@gmail.com>
2022-06-10 15:21:44 +0300

    Update solution_02

--- lesson_08/solution_02.py
+++ lesson_08/solution_02.py
@@ -9,5 +9,6 @@ from solution_01 import Car
 car_01 = Car("Mercedes", "E500", 2000)
 
 while car_01.speed < 100:
-    car_01.speed += 5
+    car_01.increase_speed()
+
 car_01.display_speed()    # Current car speed: 100
