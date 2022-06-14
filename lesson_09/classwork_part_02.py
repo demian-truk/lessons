2d28169a251e5c76017a94c1426d4b51be51a5aa
demian-truk <dimtruk93@gmail.com>
2022-06-14 17:52:56 +0300

    Update classwork_part_02

--- lesson_09/classwork_part_02.py
+++ lesson_09/classwork_part_02.py
@@ -4,3 +4,16 @@
 
 2. Создать объект класса MyDateTime, умножить его на 2 и вывести на печать результат.
 """
+
+from __future__ import annotations
+from classwork_part_01 import MyTime
+
+
+class MyDateTime(MyTime):
+
+    def __init__(self, day, month, year, hours: int, minutes: int, seconds: int):
+        super().__init__(hours, minutes, seconds)
+        self.day = day
+        self.month = month
+        self.day = day
+
