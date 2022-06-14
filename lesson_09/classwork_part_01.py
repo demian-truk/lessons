e4ae0d53df0f7847313bc942e68bbbf0089c8c30
demian-truk <dimtruk93@gmail.com>
2022-06-14 15:47:38 +0300

    Update classwork_part_01

--- lesson_09/classwork_part_01.py
+++ lesson_09/classwork_part_01.py
@@ -5,6 +5,8 @@
 2. Переопределить магические методы сложения, вычитания, умножения на число.
 
 3. Создать метод, который выводит на экран отформатированное время (HH:MM:SS).
+
+4. Создать объект класса MyTime, умножить его на 2 и вывести на печать результат.
 """
 
 from __future__ import annotations
@@ -53,8 +55,8 @@ class MyTime:
         seconds = self.to_seconds() - other.to_seconds()
         return MyTime.seconds_to_time(seconds)
 
-    def __mul__(self, other: MyTime) -> MyTime:
-        seconds = self.to_seconds() * other.to_seconds()
+    def __mul__(self, other: int) -> MyTime:
+        seconds = self.to_seconds() * other
         return MyTime.seconds_to_time(seconds)
 
     def display_time(self):
@@ -64,9 +66,10 @@ class MyTime:
             self.minutes = f"0{self.minutes}"
         if self.seconds < 10:
             self.seconds = f"0{self.seconds}"
-
         print(f"{self.hours}:{self.minutes}:{self.seconds}")
 
 
-time_01 = MyTime(1, 33, 5)
-time_01.display_time()
+if __name__ == "__main__":
+    time_01 = MyTime(1, 32, 5)
+    time_01 *= 2
+    time_01.display_time()
