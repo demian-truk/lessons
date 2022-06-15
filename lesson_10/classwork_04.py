ec878a48467375f1257d905d4ff856e3bc47ec6c
demian-truk <dimtruk93@gmail.com>
2022-06-15 21:51:32 +0300

    Update classwork_04

--- lesson_10/classwork_04.py
+++ lesson_10/classwork_04.py
@@ -1,3 +1,29 @@
 """
-Пользователь вводит два числа N и M, рассчитать последовательность  N + NN + NNN + ... + N^M.
+Пользователь вводит два числа N и M, рассчитать последовательность: N + NN + NNN + ... + NxM.
 """
+
+
+class SymbolsIterator:
+
+    def __init__(self, symbol: str, count: int):
+        self.symbol = symbol
+        self.current_value = symbol
+        self.count = count
+
+    def __next__(self) -> str:
+        previous_value = self.current_value
+        if len(self.current_value) <= self.count:
+            self.current_value += self.symbol
+            return previous_value
+        else:
+            raise StopIteration
+
+    def __iter__(self):
+        self.current_value = self.symbol
+        return self
+
+
+if __name__ == "__main__":
+    my_symb = SymbolsIterator(symbol="N", count=5)
+    for item in my_symb:
+        print(item)
