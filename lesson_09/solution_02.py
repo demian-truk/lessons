53a153ddd648c7a23569a877510abba6f3012798
demian-truk <dimtruk93@gmail.com>
2022-06-15 20:30:59 +0300

    Update solution_02

--- lesson_09/solution_02.py
+++ lesson_09/solution_02.py
@@ -4,14 +4,15 @@
 
 from solution_01 import Point, Circle, Triangle, Square
 
-figures = [
-    Circle(Point(4, 7), 3),
-    Triangle(Point(2, 2), Point(6, 2), Point(6, 5)),
-    Square(Point(2, 2), Point(7, 2)),
-]
+if __name__ == "__main__":
+    figures = [
+        Circle(Point(4, 7), 3),
+        Triangle(Point(2, 2), Point(6, 2), Point(6, 5)),
+        Square(Point(2, 2), Point(7, 2)),
+    ]
 
-for figure in figures:
-    print(f"Area of {figure.__class__.__name__}: {figure.area()}")
-    # Area of Circle: 28.274333882308138
-    # Area of Triangle: 6.0
-    # Area of Square: 25.0
+    for figure in figures:
+        print(f"Area of {figure.__class__.__name__}: {figure.area()}")
+        # Area of Circle: 28.274333882308138
+        # Area of Triangle: 6.0
+        # Area of Square: 25.0
