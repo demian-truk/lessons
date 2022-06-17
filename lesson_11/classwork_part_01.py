b553177fc6812767f6296c8b378c13fd5514af12
demian-truk <dimtruk93@gmail.com>
2022-06-17 21:59:08 +0300

    Update classwork_part_01

--- lesson_11/classwork_part_01.py
+++ lesson_11/classwork_part_01.py
@@ -4,10 +4,24 @@
 
 # 2:
 Используя консоль, создать базу sqlite3.
-В базе создать таблицу пользователей, добавить новую запись, прочитать её и удалить.
+В базе создать таблицу пользователей, добавить новую запись, прочитать её и изменить.
 Подключить базу в PyCharm.
 
 # 3:
-Создать python функцию, которая создает таблицу user, для примера использовать слайд №12.
-Запустить функцию и проверить, что создался файл базы данных.
+Создать функцию, которая позволяет добавлять данные в таблицу user. Добавить 5 различных записей.
 """
+
+import sqlite3
+
+
+def create_user(firstname: str, lastname: str, email: str, password: str, age: int):
+    with sqlite3.connect("my_database.sqlite3") as session:
+        cursor = session.cursor()
+        cursor.execute(
+            """
+            INSERT INTO user (firstname, lastname, email, password, age)
+            VALUES (?, ?, ?, ?, ?);
+            """,
+            (firstname, lastname, email, password, age),
+        )
+        session.commit()
