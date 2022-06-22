46c728011cbc79898521e5c42a25f168fb2c86e3
demian-truk <dimtruk93@gmail.com>
2022-06-22 12:32:06 +0300

    Update classwork_part_01

--- lesson_11/classwork_part_01.py
+++ lesson_11/classwork_part_01.py
@@ -4,7 +4,7 @@
 
 # 2:
 Используя консоль, создать базу sqlite3.
-В базе создать таблицу пользователей, добавить новую запись, прочитать её и изменить.
+В базе создать таблицу user, для примера использовать слайд №12, добавить новую запись, прочитать её и изменить.
 Подключить базу в PyCharm.
 
 # 3:
@@ -25,3 +25,11 @@ def create_user(firstname: str, lastname: str, email: str, password: str, age: i
             (firstname, lastname, email, password, age),
         )
         session.commit()
+
+
+if __name__ == "__main__":
+    create_user("Levi", "Ackerman", "levilevilevi@gmail.com", "TestPass2", 31)
+    create_user("Mikasa", "Ackerman", "cutemikasssa@gmail.com", "TestPass3", 22)
+    create_user("Historia", "Reiss", "reissispower@gmail.com", "TestPass4", 20)
+    create_user("Reiner", "Braun", "fatfatreinr25@gmail.com", "TestPass5", 25)
+    create_user("Armin", "Arlert", "arminarlert-anime@gmail.com", "TestPass6", 21)
