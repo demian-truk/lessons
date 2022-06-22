ade208ce00321b6f86f34cedee7792e5ffd49494
demian-truk <dimtruk93@gmail.com>
2022-06-22 14:27:22 +0300

    Update solution_01

--- lesson_11/solution_01.py
+++ lesson_11/solution_01.py
@@ -4,3 +4,16 @@
 """
 
 import sqlite3
+
+
+def create_product(name: str, price: int, amount: int, comment):
+    with sqlite3.connect("products.sqlite3") as session:
+        cursor = session.cursor()
+        cursor.execute(
+            """
+            INSERT INTO my_products (name, price, amount, comment)
+            VALUES (?, ?, ?, ?);
+            """,
+            (name, price, amount, comment),
+        )
+        session.commit()
