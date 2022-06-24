4053d8003a0283ecd3f3fbaf4fd43c5a094a6395
demian-truk <dimtruk93@gmail.com>
2022-06-24 10:34:27 +0300

    Update solution_02

--- lesson_11/solution_02.py
+++ lesson_11/solution_02.py
@@ -2,9 +2,44 @@
 Создать программу с пользовательским интерфейсом, позволяющим выбирать определенную функцию и вводить требуемые данные.
 """
 
-import sqlite3
 from solution_01 import create_product, select_product, update_product, delete_product
+
+DB_NAME = "products.sqlite3"
+
+TEMPLATE = """
+    Choose one of options:
+    1. Create new product
+    2. Output product information
+    3. Update by ID
+    4. Delete bu ID
+"""
 
 
 def user_menu():
-    pass
+    while True:
+        try:
+            user_choice = int(input(TEMPLATE))
+        except ValueError:
+            user_choice = None
+
+        if user_choice == 1:
+            print("Enter name, price, amount and comment separated by commas: ")
+            name, price, amount, comment = input().split(',')
+            create_product(name, int(price), int(amount), comment)
+        elif user_choice == 2:
+            for product in select_product():
+                print(product)
+        elif user_choice == 3:
+            print("Enter product_id, name, price, amount and comment by commas for update: ")
+            product_id, name, price, amount, comment = input().split(',')
+            update_product(int(product_id), name, int(price), int(amount), comment)
+        elif user_choice == 4:
+            print("Enter product_id for delete: ")
+            product_id = int(input())
+            delete_product(product_id)
+        else:
+            break
+
+
+if __name__ == "__main__":
+    user_menu()
