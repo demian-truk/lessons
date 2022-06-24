11041544785489939335e00614491f39fc423888
demian-truk <dimtruk93@gmail.com>
2022-06-24 15:21:17 +0300

    Add user_interface

---
+++ lesson_12/homework/user_interface.py
@@ -0,0 +1,13 @@
+"""
+Создать программу с пользовательским интерфейсом, позволяющим выбирать определенную функцию и вводить требуемые данные.
+"""
+
+from main import create_product, select_products
+
+TEMPLATE = """
+    Choose one of options:
+    1. Create new product
+    2. Output products information
+    3. Update by ID
+    4. Delete by ID
+"""
