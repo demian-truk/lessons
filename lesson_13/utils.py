679c807e1eb0cbc30ddc783ba166037a70ec72a0
demian-truk <dimtruk93@gmail.com>
2022-06-24 21:11:42 +0300

    Add utils

---
+++ lesson_13/utils.py
@@ -0,0 +1,23 @@
+"""
+Создать функции для подключения и создания базы данных.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy_utils import create_database, database_exists
+
+DB_USER = "osenstarosti"
+DB_PASSWORD = "neurosmetanka808"
+DB_NAME = "my_database"
+DB_ECHO = True
+
+
+def setup_db_engine() -> Engine:
+    engine = create_engine(
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}", echo=True,
+    )
+
+
+def create_database_if_not_exists(engine: Engine):
+    if not database_exists(engine.url):
+        create_database(engine.url)
